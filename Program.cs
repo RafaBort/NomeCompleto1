f3c5bed683b4c79767efde13b19b1b90252d1604
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-15 19:57:02 -0300

    exercicíos resolvidos

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NomeCompleto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome...");
+
+            string NOME;
+            NOME = Console.ReadLine();
+
+            Console.WriteLine("digite seu sobrenome...");
+
+            string sobrenome;
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine("Obrigada," + NOME + " " + sobrenome + "!!!");
+
+        }
+    }
+}
